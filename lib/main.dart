cf595f13d98d8b65b5ff65d93129c19d9f442070
Julian Benedikt Heuschen <julian@fheuschen.de>
2020-03-17 15:39:11 +0100

    Changed app icon and display name

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class GagernCloudApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gagern Cloud',
       theme: ThemeData(
         // This is the theme of your application.
         //
